28437a2d9f693f6d9084db72b9bf3c2318115933
ahalkaya <alkaya.alihan@gmail.com>
2017-08-16 17:42:49 +0300

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const {ObjectID} = require('mongodb');
+const {
+    ObjectID
+} = require('mongodb');
+const _ = require('lodash');
 
-let {mongoose} = require('./db/mongoose');
-let {Todo} = require('./models/todo');
-let {User} = require('./models/user');
+let {
+    mongoose
+} = require('./db/mongoose');
+let {
+    Todo
+} = require('./models/todo');
+let {
+    User
+} = require('./models/user');
 
 let app = express();
 const port = process.env.PORT || 3000;
@@ -50,7 +59,9 @@ app.get('/todos/:id', (req, res) => {
                 return res.status(404).send();
             }
 
-            res.send({todo});
+            res.send({
+                todo
+            });
         }).catch((e) => {
             res.status(400).send();
         });
@@ -69,8 +80,46 @@ app.delete('/todos/:id', (req, res) => {
             if (!todo) {
                 return res.status(404).send();
             }
-            res.send({todo});
+            res.send({
+                todo
+            });
         }).catch((e) => {
+            res.status(400).send();
+        });
+});
+
+app.patch('/todos/:id', (req, res) => {
+    let id = req.params.id;
+    let body = _.pick(req.body, ['text', 'completed']);
+
+    let isIdValid = ObjectID.isValid(id);
+    if (!isIdValid) {
+        return res.status(404).send();
+    }
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo
+        .findByIdAndUpdate(id, {
+            $set: body
+        }, {
+            new: true
+        })
+        .then((todo) => {
+            if (!todo) {
+                return res.status(404).send();
+            }
+
+            res.send({
+                todo
+            });
+        })
+        .catch((e) => {
             res.status(400).send();
         });
 });
